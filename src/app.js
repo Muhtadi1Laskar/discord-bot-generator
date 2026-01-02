f1167f32f1aa40e2f398e7526b28b929538aed19
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-01-03 02:55:43 +0600

    Added the routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import dotenv from 'dotenv';
+import { errorHandler } from "./middlewares/errorHandler.js";
+import router from "./routes/index.js";
 
 const app = express();
 
